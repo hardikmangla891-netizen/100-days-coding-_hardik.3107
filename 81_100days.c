89f2f183865b0104684cbc2e422f42ac7a290662
hardik <hardikmangla891@gmail.com>
2026-09-19 21:45:37 +0530

    Add files via upload

---
+++ 81_100days.c
@@ -0,0 +1,32 @@
+//Q81: Count characters in a string without using built-in length functions.
+
+/*
+Sample Test Cases:
+Input 1:
+Hello
+Output 1:
+5
+
+Input 2:
+ 
+Output 2:
+1
+
+*/
+#include <stdio.h>
+
+int main() {
+    char str[1000];
+    int i = 0, count = 0;
+
+    fgets(str, sizeof(str), stdin);
+
+    while (str[i] != '\0' && str[i] != '\n') {
+        count++;
+        i++;
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
---
+++ 82_100days.c
@@ -0,0 +1,25 @@
+//Q82: Print each character of a string on a new line.
+
+/*
+Sample Test Cases:
+Input 1:
+Hi
+Output 1:
+H
+i
+
+*/
+#include <stdio.h>
+
+int main() {
+    char str[1000];
+    int i;
+
+    fgets(str, sizeof(str), stdin);
+
+    for(i = 0; str[i] != '\0' && str[i] != '\n'; i++) {
+        printf("%c\n", str[i]);
+    }
+
+    return 0;
+}
